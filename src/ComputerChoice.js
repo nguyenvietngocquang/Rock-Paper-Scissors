21f3f489ef8d11e645d6f815a717b2b5cc7b9897
Nguyen Viet Ngoc Quang 20163315 <quang.nvn163315@sis.hust.edu.vn>
2020-04-02 23:54:09 +0700

    Update GUI

--- src/ComputerChoice.js
+++ src/ComputerChoice.js
@@ -4,19 +4,19 @@ export const ComputerChoice = props => {
   const { rock, paper, scissors, computerChoice } = props.allstates;
   if (computerChoice === 1) {
     return (
-      <div className="result">
+      <div className="computer-choice">
         <div>Computer</div> <img alt="Rock" width="70%" src={rock} />
       </div>
     );
   } else if (computerChoice === 2) {
     return (
-      <div className="result">
+      <div className="computer-choice">
         <div>Computer</div> <img alt="Paper" width="70%" src={paper} />
       </div>
     );
   } else if (computerChoice === 3) {
     return (
-      <div className="result">
+      <div className="computer-choice">
         <div>Computer</div> <img alt="Scissors" width="70%" src={scissors} />
       </div>
     );
--- src/YourChoice.js
+++ src/YourChoice.js
@@ -4,19 +4,19 @@ export const YourChoice = props => {
   const { rock, paper, scissors, yourChoice } = props.allstates;
   if (yourChoice === 1) {
     return (
-      <div className="result">
+      <div className="your-choice">
         <div>You</div> <img alt="Rock" width="70%" src={rock} />
       </div>
     );
   } else if (yourChoice === 2) {
     return (
-      <div className="result">
+      <div className="your-choice">
         <div>You</div> <img alt="Paper" width="70%" src={paper} />
       </div>
     );
   } else if (yourChoice === 3) {
     return (
-      <div className="result">
+      <div className="your-choice">
         <div>You</div> <img alt="Scissors" width="70%" src={scissors} />
       </div>
     );
