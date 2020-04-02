d2b7d47aede7bba79e2c8fa679126dffc76dccc7
nguyenvietngocquang <nguyenvietngocquang@gmail.com>
2020-04-02 18:31:37 +0700

    Updates

---
+++ precache-manifest.f15f0d5bd673444bc54bec4bd4f78bf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62fb009e13e3a804283b9a867114e160",
+    "url": "/Rock-Paper-Scissors/index.html"
+  },
+  {
+    "revision": "a69f53839093ea18a483",
+    "url": "/Rock-Paper-Scissors/static/css/main.3d11e6f5.chunk.css"
+  },
+  {
+    "revision": "dc7fa986857905695dc6",
+    "url": "/Rock-Paper-Scissors/static/js/2.9366f05e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Rock-Paper-Scissors/static/js/2.9366f05e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a69f53839093ea18a483",
+    "url": "/Rock-Paper-Scissors/static/js/main.7a17f7f6.chunk.js"
+  },
+  {
+    "revision": "ff46e999c0d8abb67baa",
+    "url": "/Rock-Paper-Scissors/static/js/runtime-main.6d41d3e4.js"
+  }
+]);
